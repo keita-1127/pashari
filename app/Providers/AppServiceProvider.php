73100ad4314e91094000c77880567b1f52843e43
Takayuki Yoshikawa <yoshikawa@64kb.net>
2020-03-18 16:48:00 +0900

    「production」環境以外だった場合、SQLログを出力する。

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +26,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ('production' !== config('app.env')) {
+            // 「production」環境以外だった場合、SQLログを出力する。
+            DB::listen(static function ($query): void {
+                Log::info("Query Time:{$query->time}s] {$query->sql}");
+            });
+        }
     }
 }
