d6782eb1047cec40eaab26b1c47829033460ab57
Takayuki Yoshikawa <yoshikawa@64kb.net>
2020-03-18 17:32:57 +0900

    IDE Helperによるモデルの自動ドキュメント生成

--- app/User.php
+++ app/User.php
@@ -4,10 +4,41 @@ declare(strict_types = 1);
 
 namespace App;
 
+use Eloquent;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 
+/**
+ * App\User
+ *
+ * @property int         $id
+ * @property string      $name
+ * @property string      $email
+ * @property null|Carbon $email_verified_at
+ * @property string      $password
+ * @property null|string $remember_token
+ * @property null|Carbon $created_at
+ * @property null|Carbon $updated_at
+ * @property-read DatabaseNotification[]|DatabaseNotificationCollection $notifications
+ * @property-read null|int $notifications_count
+ * @method static Builder|User newModelQuery()
+ * @method static Builder|User newQuery()
+ * @method static Builder|User query()
+ * @method static Builder|User whereCreatedAt($value)
+ * @method static Builder|User whereEmail($value)
+ * @method static Builder|User whereEmailVerifiedAt($value)
+ * @method static Builder|User whereId($value)
+ * @method static Builder|User whereName($value)
+ * @method static Builder|User wherePassword($value)
+ * @method static Builder|User whereRememberToken($value)
+ * @method static Builder|User whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class User extends Authenticatable
 {
     use Notifiable;
