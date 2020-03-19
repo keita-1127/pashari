556049f842a916be8bf573f8209fd19ba8c1690e
Takayuki Yoshikawa <yoshikawa@64kb.net>
2020-03-19 14:03:01 +0900

    「laravel/dusk」の追加

---
+++ tests/Browser/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class ExampleTest extends DuskTestCase
+{
+    /**
+     * A basic browser test example.
+     *
+     * @return void
+     */
+    public function testBasicExample(): void
+    {
+        $this->browse(function (Browser $browser): void {
+            $browser->visit('/')
+                ->assertSee('Laravel');
+        });
+    }
+}
